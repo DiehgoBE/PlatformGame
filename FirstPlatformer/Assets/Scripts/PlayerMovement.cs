3166b95db16c8c34a8dd39a9d9d881d9cac9d9c6
Diego Bravo <diegobraes21@gmail.com>
2021-01-06 11:46:44 -0500

    Caminado y salto del personaje

---
+++ FirstPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rbPlayer;
+    public float speed = .5f;
+    public float jumpSpeed = 300f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbPlayer.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rbPlayer.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rbPlayer.AddForce(Vector2.up * jumpSpeed);
+        }
+
+
+    }
+}
