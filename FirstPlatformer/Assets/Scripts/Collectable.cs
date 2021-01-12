9b4b0233a61e322732dfbdba68782491521eb288
Diego Bravo <diegobraes21@gmail.com>
2021-01-12 13:52:21 -0500

    Objectos coleccionables y UI

---
+++ FirstPlatformer/Assets/Scripts/Collectable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Collectable : MonoBehaviour
+{
+    int collectableQuantity = 0;
+    public Text collectableText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+
+            collectableQuantity++;
+            collectableText.text = collectableQuantity.ToString();
+
+
+        }
+    }
+
+}
